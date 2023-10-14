2b19f7a7899ea814dc85ea5377d9a335fc9f81e1
Aaron Milne <2466574M@student.gla.ac.uk>
2023-10-14 21:46:04 +0100

    modified trigger function

--- ElectrotactileFeedback/WidgetWebApp/app/helpers.py
+++ ElectrotactileFeedback/WidgetWebApp/app/helpers.py
@@ -1,12 +1,20 @@
+from asyncio.windows_events import NULL
 import FESDriver as fes
 
 #Send pulse to the device with the given parameters
-def send_pulse(pulsewidth, frequency, amplitude, port='COM3', baudrate=38400):
+def send_pulse(pulsewidth, frequency, amplitude, channels=None, port='COM3', baudrate=38400):
     device = fes.FESDriver(port, baudrate)
     device.connect()
-    device.set_global_pulsewidth(pulsewidth)
-    device.set_global_frequency(frequency)
-    device.set_global_amplitude(amplitude)
+    if channels is None:
+        device.set_global_pulsewidth(pulsewidth)
+        device.set_global_frequency(frequency)
+        device.set_global_amplitude(amplitude)
+    else:
+        for channel in channels:
+            device.set_channel_pulsewidth(channel, pulsewidth)
+            device.set_channel_frequency(channel, frequency)
+            device.set_channel_amplitude(channel, amplitude)
+
     device.reset()
     device.disconnect()
 
