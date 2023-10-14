e2be65ff9a0ef93baea291609cc4b068ae10a132
Aaron Milne <2466574M@student.gla.ac.uk>
2023-10-14 19:10:17 +0100

    Finished setup of project. Created form with couple different widgets Displaying this on main page of web app. Commented out some unnecessary code which can later be removed. #1

--- ElectrotactileFeedback/WidgetWebApp/app/forms.py
+++ ElectrotactileFeedback/WidgetWebApp/app/forms.py
@@ -4,7 +4,21 @@ Definition of forms.
 
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm
-from django.utils.translation import gettext_lazy as _
+from django.utils.translation import gettext_lazy as _ 
+
+
+#TODO: edit the name of this/potentially change structure.
+class MainForm(forms.Form):
+    options = ( 
+        ("1", "One"), 
+        ("2", "Two"), 
+        ("3", "Three"), 
+        ("4", "Four"), 
+        ("5", "Five"), 
+    )
+    radio_options = forms.ChoiceField(choices=options, widget=forms.RadioSelect, label="Try selecting an option")
+    test = forms.CharField(label="Enter your text", max_length=100)
+
 
 class BootstrapAuthenticationForm(AuthenticationForm):
     """Authentication form which uses boostrap CSS."""
