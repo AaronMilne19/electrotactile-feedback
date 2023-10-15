0db13edcd47e4a515400be7f00784f89eb376d9c
Aaron Milne <2466574M@student.gla.ac.uk>
2023-10-15 15:51:59 +0100

    some slight refactorings

--- ElectrotactileFeedback/WidgetWebApp/app/forms.py
+++ ElectrotactileFeedback/WidgetWebApp/app/forms.py
@@ -6,7 +6,6 @@ from django import forms
 from django.contrib.auth.forms import AuthenticationForm
 from django.utils.translation import gettext_lazy as _ 
 
-
 #TODO: edit the name of this/potentially change structure.
 class MainForm(forms.Form):
     options = ( 
@@ -18,9 +17,10 @@ class MainForm(forms.Form):
     )
     radio_options = forms.ChoiceField(choices=options, widget=forms.RadioSelect(attrs={'onclick' : "triggerDevice('radio');"}), label="Try selecting an option")
     checkbox_options = forms.ChoiceField(choices=options, widget=forms.CheckboxSelectMultiple(attrs={'onclick' : "triggerDevice('checkbox');"}), label="Try selecting multiple options")
-    test = forms.CharField(label="Enter your text", max_length=100)
+    text_input = forms.CharField(label="Enter your text", max_length=100)
 
 
+#Some pre-existing authentication stuff
 class BootstrapAuthenticationForm(AuthenticationForm):
     """Authentication form which uses boostrap CSS."""
     username = forms.CharField(max_length=254,
